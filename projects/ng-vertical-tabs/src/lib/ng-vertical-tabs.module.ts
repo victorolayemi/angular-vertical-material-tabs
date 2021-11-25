8f109a60c46f93312c516948e1c7ae720a895f33
victorolayemi <victorolayemi@users.noreply.github.com>
2021-11-25 14:54:32 +0100

    Update ng-vertical-tabs.module.ts

--- projects/ng-vertical-tabs/src/lib/ng-vertical-tabs.module.ts
+++ projects/ng-vertical-tabs/src/lib/ng-vertical-tabs.module.ts
@@ -4,7 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { MatButtonModule, MatDividerModule, MatListModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+
+import { MatDividerModule } from '@angular/material/divider';
 
 import { NgVerticalTabsComponent } from './ng-vertical-tabs/ng-vertical-tabs.component';
 import { DynamicTabAnchorDirective } from './dynamic-tab-anchor.directive';
@@ -25,4 +28,11 @@ export class NgVerticalTabsModule {
   public static forRoot(): ModuleWithProviders {
     return { ngModule: NgVerticalTabsModule, providers: [NgVerticalTabsService] };
   }
+}
+
+declare module "@angular/core" {
+    interface ModuleWithProviders<T = any> {
+        ngModule: Type<T>;
+        providers?: Provider[];
+    }
 }
